040b916428da4013da8367be98427e6861ee3efe
Paulo Henrique <binhoph@gmail.com>
2014-03-10 11:49:10 -0300

    Pegando MAC Localhost

---
+++ PegandoMac/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PegandoMac._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <asp:Label ID="lbl" Text="text" runat="server" />
+</asp:Content>
---
+++ PegandoMac/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PegandoMac
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lbl.Text = GetMAC();
+        }
+
+        private string GetMAC()
+        {
+            string macAddresses = "";
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            return macAddresses;
+        }
+    }
+}
